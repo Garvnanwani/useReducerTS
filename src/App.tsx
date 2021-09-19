1a8a2c57852870d3ba1ad8c7318efddd4e7bcaa6
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-19 22:39:41 +0530

    counter using reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useReducer, useState } from 'react'
 import './App.css'
-import { reducer } from './reducer'
+// import { reducer } from './reducer'
 
 // useReducer is basically alternative to useState, jab tumahre paas kaafi saare states or actions ho, lie kind of what redux does
 // separating logic from components
@@ -27,21 +27,96 @@ import { reducer } from './reducer'
 
 
 // HOW TO MAKE A COUNTER USING USEREDUCER
-function App(): JSX.Element {
+// function App(){
+//   const [store, dispatch] = useReducer(reducer, {
+//     count: 0,
+//     name: "naaz"
+//   })
 
-  const [store, dispatch] = useReducer(reducer, {
-    count: 0,
-    name: "naaz"
-  })
+//   return (
+//     <div className="App">
+//       <h1>{store.count}</h1>
+//       <h1>{store.name}</h1>
+//       <button onClick={() => dispatch('increase')}>Increase</button>
+//       <button onClick={() => dispatch('decrease')}>Decrease</button>
+//     </div>
+//   )
+// }
 
-  return (
-    <div className="App">
-      <h1>{store.count}</h1>
-      <h1>{store.name}</h1>
-      <button onClick={() => dispatch('increase')}>Increase</button>
-      <button onClick={() => dispatch('decrease')}>Decrease</button>
-    </div>
-  )
+// export default App
+
+interface State{
+  count: number;
+  name: string;
+}
+type Action = "increase"|"decrease"
+
+
+const reducer = (state: State, action:Action): State =>{
+  if (action ==="increase"){
+
+      if (state.count>=0) {
+        return {
+          count: state.count+1,
+          name: "positive"
+      }
+      }
+      if (state.count<0) {
+        if(state.count===-1) {
+          return{
+            count: state.count+1,
+          name: "non-negative"
+        }
+        }
+        return {
+          count: state.count+1,
+          name: "negative"
+        }
+      }
+
+  }
+
+  if(action ==="decrease"){
+
+    if (state.count>0) {
+      if(state.count===1) {
+        return{
+          count: state.count-1,
+        name: "non-negative"
+      }
+      }
+      return {
+        count: state.count-1,
+        name: "positive"
+    }
+    }
+    if (state.count<=0) {
+      return {
+        count: state.count-1,
+        name: "negative"
+    }
+    }
+
+}
+return state
 }
 
-export default App
+
+ const App=()=>{
+
+   const store={
+     count:0,
+     name: 'non-negative'
+   }
+   const [state, dispatch] = useReducer(reducer, store)
+
+   return(
+     <div className="App">
+       <h2>{state.count}</h2>
+       <h2>{state.name}</h2>
+       <button onClick={()=>dispatch("increase")}>increase</button>
+       <button onClick={() => dispatch('decrease')}>decrease</button>
+     </div>
+   )
+ }
+ export default App;
